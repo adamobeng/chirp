a71852b8a04015168d46ea78b39f7f0ebdb6fd5d
Adam Obeng <github@binaryeagle.com>
2015-03-07 16:39:24 -0500

    Working translation
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- tw.py
+++ tw.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+from nltk.corpus import wordnet as wn
+import nltk.data
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return "<form action='/translate' method='post'><input type=text name=text></input></form>"
+
+def shortest_syn(word):
+    synset = wn.synsets(word)
+    synnames = list(s.name().split('.')[0] for s in synset) + [word]
+    shortest = min(synnames, key=len)
+    print word, synnames, shortest
+    return shortest
+
+@app.route('/translate', methods=['POST'])
+def translate():
+    text = request.form['text'].split(' ') #TODO tokenize properly
+
+    s = ' '.join(shortest_syn(w) for w in text)
+
+    return s
+
 
 if __name__ == '__main__':
     app.run(debug=True)
