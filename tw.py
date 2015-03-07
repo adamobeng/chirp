879097d29385cab50790cfdb3ac01d76e9e5de85
Adam Obeng <github@binaryeagle.com>
2015-03-07 17:26:43 -0500

    Working spellchecker
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- tw.py
+++ tw.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 from nltk.corpus import wordnet as wn
 import nltk.data
 
@@ -13,15 +13,27 @@ def shortest_syn(word):
     synnames = list(s.name().split('.')[0] for s in synset) + [word]
     shortest = min(synnames, key=len)
     print word, synnames, '=>', shortest
-    return shortest
+    if (shortest != word) and len(shortest) < len(word):
+        return shortest
+    else:
+        return None
 
 @app.route('/translate', methods=['POST'])
 def translate():
-    text = request.form['text'].split(' ') #TODO tokenize properly
+    text = request.form.get('text') or request.args.get('text')
+    text =text.split(' ') #TODO tokenize properly
 
     s = ' '.join(shortest_syn(w) for w in text)
-
     return render_template('results.html', text=s)
+
+@app.route('/spellcheck.php', methods=['GET', 'POST'])
+def spellcheck():
+    text = request.form.get('text') or request.args.get('text')
+    wordsin = text.split(' ') #TODO tokenize properly
+
+    wordsout = [{'o': text.find(w), 'l':len(w), 's':3, 'options': shortest_syn(w)} for w in wordsin if shortest_syn(w)]
+
+    return render_template('results.xml', words = wordsout, charschecked=len(text))
 
 
 if __name__ == '__main__':
